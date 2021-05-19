bfd685a19a351ee14d0fb596e64459b831667b92
Anuja Arosha Piyadigama <arosha@creativesoftware.com>
2021-05-19 15:32:55 +0530

    Bubble sort with two for loops

--- BubbleSort.playground/Contents.swift
+++ BubbleSort.playground/Contents.swift
@@ -1,3 +1,25 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+func bubbleSort(array: [Int]) -> [Int] {
+    
+    var mutableArray = array
+    
+    for _ in 0..<mutableArray.count-1 {
+        
+        for y in 0..<mutableArray.count-1 {
+            
+            if (mutableArray[y] > mutableArray[y+1]) {
+                
+                let temp = mutableArray[y]
+                mutableArray[y] = mutableArray[y+1]
+                mutableArray[y+1] = temp
+            }
+        }
+    }
+    
+    return mutableArray
+}
+
+
+let unsortedArray = [4, 5, 2, 1, 3]
+print(bubbleSort(array: unsortedArray))
