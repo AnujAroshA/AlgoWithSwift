bce0cd6f8750b26e7fc31debfec880ef37c86095
Anuja Arosha Piyadigama <arosha@creativesoftware.com>
2021-05-19 15:51:09 +0530

    Bubble sort with single for loop and repeat while loop

--- BubbleSort.playground/Contents.swift
+++ BubbleSort.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+let unsortedArray = [4, 5, 2, 1, 3]
+
 func bubbleSort(array: [Int]) -> [Int] {
     
     var mutableArray = array
@@ -20,6 +22,24 @@ func bubbleSort(array: [Int]) -> [Int] {
     return mutableArray
 }
 
-
-let unsortedArray = [4, 5, 2, 1, 3]
 print(bubbleSort(array: unsortedArray))
+
+func bubbleSort2(array: [Int]) -> [Int] {
+    
+    var isSwapped = false
+    var mutableArray = array
+    
+    repeat {
+        isSwapped = false
+        for y in 0..<mutableArray.count-1 {
+            if (mutableArray[y] > mutableArray[y+1]) {
+                mutableArray.swapAt(y+1, y)
+                isSwapped = true
+            }
+        }
+    } while isSwapped
+    
+    return mutableArray
+}
+
+print(bubbleSort2(array: unsortedArray))
